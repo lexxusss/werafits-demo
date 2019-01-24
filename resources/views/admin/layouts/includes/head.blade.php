54d69596540815bc60f8a610e41727d106ff89e2
alextsyk <alexxx.tsyk@gmail.com>
2019-01-24 15:01:21 +0100

    + rewrite layouts, + htaccess redirect

---
+++ resources/views/admin/layouts/includes/head.blade.php
@@ -0,0 +1,18 @@
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin_ws.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/lib/font-awesome.min.css') }}">
+
+    @yield('head')
+</head>
